6a80f3257279535fcbc14386ba97d9f7e15457ee
cryyterium <matthias.hacbect@gmail.com>
2026-04-27 13:34:15 +0200

    deplacement des fichiers html au niveau du fichier template et ajout dans view et url

--- hopital/hopital/urls.py
+++ hopital/hopital/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import liste_objets
+from . import views
 
 urlpatterns = [
-    path('objets/', liste_objets, name='liste_objets'),
+    path('', views.index, name='index'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register, name='register'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('profile/', views.profile, name='profile'),
+    path('objects/', views.objects, name='objects'),
+    path('room/', views.room, name='room'),
+    path('admin-page/', views.admin_page, name='admin'),
+    path('manage-users/', views.manage_users, name='manage_users'),
+    path('manage-objects/', views.manage_objects, name='manage_objects'),
+    path('stats/', views.stats, name='stats'),
 ]
--- hopital/hopital/views.py
+++ hopital/hopital/views.py
@@ -3,6 +3,42 @@ from django.shortcuts import render
 # Create your views here.
 from .models import ObjetConnecte
 
+def accueil(request):
+    return render(request, 'hopital/accueil.html')
+
 def liste_objets(request):
     objets = ObjetConnecte.objects.all()
     return render(request, 'hopital/liste_objets.html', {'objets': objets})
+
+def index(request):
+    return render(request, 'hospital/index.html')
+
+def login_view(request):
+    return render(request, 'hospital/login.html')
+
+def register(request):
+    return render(request, 'hospital/register.html')
+
+def dashboard(request):
+    return render(request, 'hospital/dashboard.html')
+
+def profile(request):
+    return render(request, 'hospital/profile.html')
+
+def objects(request):
+    return render(request, 'hospital/objects.html')
+
+def room(request):
+    return render(request, 'hospital/room.html')
+
+def admin_page(request):
+    return render(request, 'hospital/admin.html')
+
+def manage_users(request):
+    return render(request, 'hospital/manage-users.html')
+
+def manage_objects(request):
+    return render(request, 'hospital/manage-objects.html')
+
+def stats(request):
+    return render(request, 'hospital/stats.html')
