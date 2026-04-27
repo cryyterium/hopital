2b916aa568754706ac09560fb1eac522506f9932
cryyterium <matthias.hacbect@gmail.com>
2026-04-27 13:42:41 +0200

    Création statique pour mettre le css et js avec django

--- hopital/hopital/views.py
+++ hopital/hopital/views.py
@@ -11,34 +11,34 @@ def liste_objets(request):
     return render(request, 'hopital/liste_objets.html', {'objets': objets})
 
 def index(request):
-    return render(request, 'hospital/index.html')
+    return render(request, 'hopital/index.html')
 
 def login_view(request):
-    return render(request, 'hospital/login.html')
+    return render(request, 'hopital/login.html')
 
 def register(request):
-    return render(request, 'hospital/register.html')
+    return render(request, 'hopital/register.html')
 
 def dashboard(request):
-    return render(request, 'hospital/dashboard.html')
+    return render(request, 'hopital/dashboard.html')
 
 def profile(request):
-    return render(request, 'hospital/profile.html')
+    return render(request, 'hopital/profile.html')
 
 def objects(request):
-    return render(request, 'hospital/objects.html')
+    return render(request, 'hopital/objects.html')
 
 def room(request):
-    return render(request, 'hospital/room.html')
+    return render(request, 'hopital/room.html')
 
 def admin_page(request):
-    return render(request, 'hospital/admin.html')
+    return render(request, 'hopital/admin.html')
 
 def manage_users(request):
-    return render(request, 'hospital/manage-users.html')
+    return render(request, 'hopital/manage-users.html')
 
 def manage_objects(request):
-    return render(request, 'hospital/manage-objects.html')
+    return render(request, 'hopital/manage-objects.html')
 
 def stats(request):
-    return render(request, 'hospital/stats.html')
+    return render(request, 'hopital/stats.html')
