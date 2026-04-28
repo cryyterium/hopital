bb8b69083212815ab89170c00fa93b448e7a0a58
cryyterium <matthias.hacbect@gmail.com>
2026-04-28 16:21:17 +0200

    maj

--- hopital/config/settings.py
+++ hopital/config/settings.py
@@ -125,7 +125,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
-EMAIL_HOST_USER = 'matthias.hacbect@gmail.com'
-EMAIL_HOST_PASSWORD = 'yehldcoyzxupijlx'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
